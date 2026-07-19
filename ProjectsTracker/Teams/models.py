8cbe8812057130acc441e7ce5a0ee18e8de21477
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-07-19 22:16:51 +0545

    Team model basic form

--- ProjectsTracker/Teams/models.py
+++ ProjectsTracker/Teams/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class TeamsModel(models.Model):
+    
+    team_id = models.UUIDField(primary_key=True, default=uuid4)
+    name = models.CharField(max_length=20)
+    main_field_of_work = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
